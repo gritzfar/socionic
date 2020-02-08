76f732029b0cb29c1c0617e08f03fd52479f705f
G.A.S.© <gritzfar@gmail.com>
2020-02-08 23:16:25 +0100

    Add relation desc

--- lib/relationsPage.dart
+++ lib/relationsPage.dart
@@ -7,7 +7,6 @@ import 'appDrawer.dart';
 import 'main.dart';
 import 'relations.dart';
 import 'typeHero.dart';
-import 'typePage.dart';
 import 'types.dart';
 
 MaterialPageRoute<void> getTypeRelationsPage(TypeDesc typeDesc, bool popNavigation) {
@@ -180,19 +179,6 @@ class TypeRelationsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget gridSection = Expanded(child: Padding(padding: EdgeInsets.all(5), child: getRows(context)));
 
-//    Widget gridSection = Expanded(
-//        child: SingleChildScrollView(
-//            scrollDirection: Axis.vertical,
-//            physics: const BouncingScrollPhysics(),
-//            child: Padding(
-//                padding: EdgeInsets.all(5),
-//                child: Container(
-//                    child: SingleChildScrollView(
-//                  scrollDirection: Axis.horizontal,
-//                  physics: const BouncingScrollPhysics(),
-//                  child: getRows(context),
-//                )))));
-
     Widget body = new Column(
       // This makes each child fill the full width of the screen
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -247,11 +233,11 @@ class TypeRelationsPage extends StatelessWidget {
                       TypeHero(
                           typeDesc: types[i],
                           onTap: () {
-                            if (popNavigation) {
-                              //Navigator.of(context).pop();
-                            } else {
-                              Navigator.of(context).push(getTypeDescPage(types[i]));
-                            }
+                            //if (popNavigation) {
+                            Navigator.of(context).push(getRelationDescriptionPage(relations[i]));
+//                            } else {
+//                              Navigator.of(context).push(getTypeDescPage(types[i]));
+//                            }
                           })
                       //],)
                     ])))),
@@ -302,4 +288,90 @@ class TypeRelationsPage extends StatelessWidget {
               ))
         ]));
   }
+}
+
+MaterialPageRoute<void> getRelationDescriptionPage(Relation relation) {
+  return MaterialPageRoute<void>(builder: (BuildContext context) {
+    return RelationDescriptionPage(relation: relation);
+  });
+}
+
+class RelationDescriptionPage extends StatelessWidget {
+  final Relation relation;
+
+  RelationDescriptionPage({Key key, this.relation}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var color = Colors.amber;
+
+    return new Scaffold(
+        appBar: new AppBar(
+            title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[Text(relation.longName), Text(relation.moto, style: Theme.of(context).textTheme.caption)],
+        )),
+        body: CustomScrollView(physics: const BouncingScrollPhysics(), slivers: <Widget>[
+          SliverPersistentHeader(
+            pinned: true,
+            floating: true,
+            delegate: _SliverAppBarDelegate(relation: relation),
+          ),
+          SliverToBoxAdapter(
+              child: Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+            child: Column(children: <Widget>[
+              Text(relation.desc ?? "TEST"),
+            ]),
+          ))
+        ]));
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  final Relation relation;
+
+  _SliverAppBarDelegate({this.relation});
+
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
+    var offset = maxExtent - shrinkOffset < minExtent ? minExtent - maxExtent : -shrinkOffset;
+
+    return Stack(overflow: Overflow.clip, children: <Widget>[
+      Positioned(
+          top: offset,
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: maxExtent,
+              child: Container(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  padding: EdgeInsets.only(left: 16, top: 5, right: 16, bottom: 0),
+                  child: _getTopBar(context, relation))))
+    ]);
+  }
+
+  @override
+  double get maxExtent => 60;
+
+  @override
+  double get minExtent => 5;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
+
+Widget _getTopBar(BuildContext context, Relation relation) {
+  return Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+    StarRating(
+      color: Colors.amber,
+      rating: relation.rating,
+      size: 40,
+    ),
+    Divider(
+      height: 5,
+    )
+  ]);
 }
